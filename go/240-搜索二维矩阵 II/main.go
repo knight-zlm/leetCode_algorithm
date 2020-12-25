f815d0df413ccf6865bab88bf89c22d82169df97
zhoulm <zhoulm@inmindglobal.com>
2020-12-25 21:21:43 +0800

    240题解

---
+++ go/240-搜索二维矩阵 II/main.go
@@ -0,0 +1,46 @@
+package main
+
+/**
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+
+示例 1：
+输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+输出：true
+
+示例 2：
+输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+输出：false
+
+提示：
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matix[i][j] <= 109
+每行的所有元素从左到右升序排列
+每列的所有元素从上到下升序排列
+-109 <= target <= 109
+*/
+
+func searchMatrix(matrix [][]int, target int) bool {
+	// 右下角作为起点
+	row := len(matrix) - 1
+	column := 0
+	for row >= 0 && row < len(matrix) && column >= 0 && column < len(matrix[0]) {
+		if matrix[row][column] == target {
+			return true
+		}
+		if matrix[row][column] > target {
+			row--
+		} else {
+			column++
+		}
+	}
+
+	return false
+}
+
+func main() {
+
+}
