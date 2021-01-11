92ccb511faf34d37dd4a40865563ef35b7227f0e
zhoulm <zhoulm@inmindglobal.com>
2021-01-11 22:14:41 +0800

    337解题

---
+++ go/337-打家劫舍 III/main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+/**
+在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+示例 1:
+输入: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \
+     3   1
+输出: 7
+解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+
+示例 2:
+输入: [3,4,5,1,3,null,1]
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rob(root *TreeNode) int {
+	// 打劫根 + 左边不带根的最大和右边不带根的最大
+	// 左边带根的最大值 + 右边带根的最大
+	result := solver(root)
+	return max(result[0], result[1])
+}
+
+func solver(root *TreeNode) [2]int {
+	if root == nil {
+		return [2]int{}
+	}
+	lResult := solver(root.Right)
+	rResult := solver(root.Left)
+	val1 := root.Val + lResult[1] + rResult[1]
+	val2 := max(lResult[0], lResult[1]) + max(rResult[0], rResult[1])
+	return [2]int{val1, val2}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	data := &TreeNode{
+		Val: 4,
+		Left: &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+			Right: nil,
+		},
+		Right: nil,
+	}
+	fmt.Println(rob(data))
+}
